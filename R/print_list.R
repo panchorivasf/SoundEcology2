bd06d8547975f6282de9ef7d396e178e5123b4bd
Francisco Rivas <fcorivasf@gmail.com>
2024-10-01 19:15:55 -0400

    updated print_list

--- R/print_list.R
+++ R/print_list.R
@@ -5,8 +5,8 @@
 #' @return a printed list.
 #' @export
 #'
-#' @examples print.list(list)
-print.list <- function(list){
+#' @examples print_list(list)
+print_list <- function(list){
   for (i in seq_along(list)) {
     cat(i, list[[i]], "\n")
   }

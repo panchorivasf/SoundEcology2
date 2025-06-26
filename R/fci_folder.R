c232617c9855e6ea0ac45c94e4df30be5c799798
panchorivasf <fcorivasf@gmail.com>
2025-06-26 19:00:47 -0400

    add start and end to fci_folder

--- R/fci_folder.R
+++ R/fci_folder.R
@@ -4,8 +4,13 @@
 #' It processes each sound file in parallel (if specified) and saves the results in a CSV file.
 #'
 #' @param folder The folder containing the WAV files to analyze.
+#' @param recursive Logical. Whether to search in subfolders. Default is TRUE.
 #' @param list An optional list (subset) of files in the folder to analyze. If provided, 
 #' files outside the list will be excluded. 
+#' @param start numerical. Where to start reading the Wave. 
+#' @param end numerical. Where to end reading the Wave.
+#' @param unit character. Unit of measurement for 'start' and 'end'. Options are
+#' 'samples', 'seconds', 'minutes', 'hours'. Default is 'minutes'.
 #' @param output.csv The name of the CSV file where results will be saved. Default is "frequency_cover_results.csv".
 #' @param channel The channel to analyze: 'left', 'right', 'mix' (combine both), or 'each' (process left and right channels separately). Default is 'left'.
 #' @param hpf High-pass filter cutoff frequency in Hz. If 0, no high-pass filter is applied. Default is 0.
@@ -40,7 +45,11 @@
 #' }
 
 fci_folder <- function(folder = NULL,
+                       recursive = FALSE,
                        list = NULL,
+                       start = 0,
+                       end = 1,
+                       unit = "minutes",
                        output.csv = "fci_results.csv",
                        channel = 'each',
                        hpf = 0,
